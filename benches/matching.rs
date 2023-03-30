a428ceb37f71a6501c0bcd4765d0fcb248297587
Simeon Widdis <sawiddis@gmail.com>
2023-03-29 20:32:23 -0700

    Reformat

--- benches/matching.rs
+++ benches/matching.rs
@@ -5,10 +5,12 @@ fn letter_dot_benchmark(c: &mut Criterion) {
     let wordlist = ["lone", "love", "word", "door", "dome", "lint", "leftie"];
     let mut i = 0;
     let pattern = Pattern::new("l..e");
-    c.bench_function("letter-dot-1", |b| b.iter(|| {
-        pattern.matches(black_box(wordlist[i]));
-        i = (i + 1) % wordlist.len();
-    }));
+    c.bench_function("letter-dot-1", |b| {
+        b.iter(|| {
+            pattern.matches(black_box(wordlist[i]));
+            i = (i + 1) % wordlist.len();
+        })
+    });
 }
 
 criterion_group!(benches, letter_dot_benchmark);
