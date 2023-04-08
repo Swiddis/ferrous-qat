166b9a2a3ac6b69e218a677824c4c382029918f4
Simeon Widdis <sawiddis@gmail.com>
2023-04-08 16:09:08 -0700

    Re-implement sets

--- src/pattern/mod.rs
+++ src/pattern/mod.rs
@@ -1,6 +1,8 @@
 pub mod charset;
 
-use self::charset::{CharSet, EN_CONSONANTS, EN_VOWELS};
+use pest::iterators::Pair;
+
+use self::charset::{CharSet, EN_CONSONANTS, EN_VOWELS, EnCharSet};
 use crate::pest::{error::Error, Parser};
 
 use super::parsing::{QatParser, Rule};
@@ -8,6 +10,8 @@ use super::parsing::{QatParser, Rule};
 #[derive(Debug)]
 enum Node {
     Letter(char),
+    Set(EnCharSet),
+    NegSet(EnCharSet),
     Any,
     Vowel,
     Consonant,
@@ -19,6 +23,23 @@ pub struct Pattern {
 }
 
 impl Pattern {
+    fn make_set(pair: Pair<Rule>) -> EnCharSet {
+        let mut set = EnCharSet::new();
+        pair.into_inner().for_each(|p| match p.as_rule() {
+            Rule::letter => set.insert(p.as_str().chars().next().unwrap()),
+            Rule::set_range => {
+                let mut chars = p.as_str().chars();
+                let left = chars.next().unwrap();
+                let right = chars.skip(1).next().unwrap();
+                for c in left..=right {
+                    set.insert(c);
+                }
+            },
+            r => panic!("invalid set contents {:?}", r),
+        });
+        set
+    }
+
     pub fn new(source: &str) -> Result<Self, Error<Rule>> {
         let tree = QatParser::parse(Rule::pattern, source)?.next().unwrap();
         let nodes = tree
@@ -28,6 +49,8 @@ impl Pattern {
                 Rule::dot => Node::Any,
                 Rule::vowel => Node::Vowel,
                 Rule::consonant => Node::Consonant,
+                Rule::set => Node::Set(Self::make_set(p)),
+                Rule::negset => Node::NegSet(Self::make_set(p)),
                 r => panic!("unrecognized rule {:?}", r),
             })
             .collect();
@@ -40,6 +63,8 @@ impl Pattern {
             .zip(word.chars())
             .all(|(node, w)| match node {
                 Node::Letter(c) => w == *c,
+                Node::Set(s) => s.contains(w),
+                Node::NegSet(s) => !s.contains(w),
                 Node::Vowel => EN_VOWELS.contains(w),
                 Node::Consonant => EN_CONSONANTS.contains(w),
                 Node::Any => true,
