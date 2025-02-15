a297922a88f09ba68c5f50effbc49dfc20633ae1
Simeon Widdis <sawiddis@gmail.com>
2025-02-15 14:25:27 -0800

    Reimplement set matching

--- src/parse.rs
+++ src/parse.rs
@@ -1,32 +1,58 @@
 // See the grammar file in docs/qat_grammar.pest
 
 use nom::{
-    bytes::complete::tag, character::complete::one_of, combinator::opt, multi::many0,
-    sequence::preceded, IResult, Parser,
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{char, one_of},
+    combinator::{into, opt},
+    multi::many0,
+    sequence::{delimited, preceded, separated_pair},
+    IResult, Parser,
 };
 
-use crate::pattern::{Element, SimplePattern};
+use crate::pattern::{Element, LetterSet, SimplePattern, ANY_LETTER};
 
-// Any char that can be exactly mapped to an Element
-const UNIT_ELEMENTS: &str = "abcdefghijklmnoqprstuvwxyz0123456789.@#*><";
-const ANY_LETTER: Element = Element::Set(0x3ffffff);
-const VOWEL: Element = Element::Set(0x104111);
-const CONSONANT: Element = Element::Set(0x3efbeee);
+const LOWERCASE: &str = "abcdefghijklmnopqrstuvwxyz";
+/// Any char that can be exactly mapped to an Element
+const UNIT_ELEMENTS: &str = "abcdefghijklmnopqrstuvwxyz0123456789.@#*><";
+
+fn unit_element(input: &str) -> IResult<&str, Element> {
+    into(one_of(UNIT_ELEMENTS)).parse(input)
+}
+
+fn set_letter(input: &str) -> IResult<&str, LetterSet> {
+    into(one_of(LOWERCASE)).parse(input)
+}
+
+fn set_range(input: &str) -> IResult<&str, LetterSet> {
+    into(separated_pair(
+        one_of(LOWERCASE),
+        tag("-"),
+        one_of(LOWERCASE),
+    ))
+    .parse(input)
+}
+
+fn set(input: &str) -> IResult<&str, Element> {
+    let (rem, (negation, subsets)) = delimited(
+        char('['),
+        (opt(char('!')), many0(alt((set_letter, set_range)))),
+        char(']'),
+    )
+    .parse(input)?;
+
+    let reduced = subsets.into_iter().reduce(|a, b| LetterSet(a.0 | b.0));
+
+    match (negation, reduced) {
+        (None, None) => Ok((rem, Element::Set(LetterSet(0)))),
+        (None, Some(s)) => Ok((rem, Element::Set(s))),
+        (Some(_), None) => Ok((rem, Element::Set(ANY_LETTER))),
+        (Some(_), Some(s)) => Ok((rem, Element::Set(LetterSet(ANY_LETTER.0 & !s.0)))),
+    }
+}
 
 fn element(input: &str) -> IResult<&str, Element> {
-    let (rem, c) = one_of(UNIT_ELEMENTS).parse(input)?;
-    let elem = match c {
-        'a'..='z' => Element::Set(1 << (c as u32 - 'a' as u32)),
-        '0'..='9' => Element::Copy(c as u8),
-        '.' => ANY_LETTER,
-        '@' => VOWEL,
-        '#' => CONSONANT,
-        '*' => Element::Wildcard,
-        '>' => Element::Word,
-        '<' => Element::RevWord,
-        _ => unreachable!(),
-    };
-    Ok((rem, elem))
+    alt((unit_element, set)).parse(input)
 }
 
 pub fn simple_pattern(input: &str) -> IResult<&str, SimplePattern> {
--- src/pattern.rs
+++ src/pattern.rs
@@ -1,8 +1,34 @@
+pub const ANY_LETTER: LetterSet = LetterSet(0x3ffffff);
+const VOWELS: LetterSet = LetterSet(0x104111);
+const CONSONANTS: LetterSet = LetterSet(0x3efbeee);
+
+#[derive(Clone, Copy, Debug)]
+pub struct LetterSet(pub u32);
+
+impl From<char> for LetterSet {
+    fn from(value: char) -> Self {
+        assert!(value.is_ascii_lowercase());
+        Self(1 << (value as u32 - 'a' as u32))
+    }
+}
+
+impl From<(char, char)> for LetterSet {
+    /// Parse the letter set as a range between the given letters
+    fn from((left, right): (char, char)) -> Self {
+        assert!(left.is_ascii_lowercase() && right.is_ascii_lowercase());
+        let (left, right) = (
+            std::cmp::min(left, right) as u32 - 'a' as u32,
+            std::cmp::max(left, right) as u32 - 'a' as u32 + 1,
+        );
+        Self(ANY_LETTER.0 << left & !(ANY_LETTER.0 << right))
+    }
+}
+
 #[derive(Debug)]
 pub enum Element {
     /// A single-character letter matcher as a bitset. Can flexibly handle
     /// 'a'..'z', '.', '@', '#', and '[...]'.
-    Set(u32),
+    Set(LetterSet),
     /// Represents hooks to match the repeated decimal digit variables '0'..'9'.
     Copy(u8),
     /// Represents '*'.
@@ -11,6 +37,22 @@ pub enum Element {
     Word,
     /// Represents '<'.
     RevWord,
+}
+
+impl From<char> for Element {
+    fn from(value: char) -> Self {
+        match value {
+            'a'..='z' => Self::Set(value.into()),
+            '0'..='9' => Self::Copy(value as u8),
+            '.' => Self::Set(ANY_LETTER),
+            '@' => Self::Set(VOWELS),
+            '#' => Self::Set(CONSONANTS),
+            '*' => Self::Wildcard,
+            '>' => Self::Word,
+            '<' => Self::RevWord,
+            _ => panic!("illegal Element character '{value}'"),
+        }
+    }
 }
 
 /// A SimplePattern can be resolved by traversing it from left to right.
@@ -25,7 +67,7 @@ impl SimplePattern {
         for (elem, c) in self.sequence.iter().zip(word.chars()) {
             match elem {
                 Element::Set(m) => {
-                    if 1 << (c as u32 - 'a' as u32) & m == 0 {
+                    if 1 << (c as u32 - 'a' as u32) & m.0 == 0 {
                         return false;
                     }
                 }
