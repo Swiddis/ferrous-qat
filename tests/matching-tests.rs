2b4c8fd6fab923dbf07e57c40189e81fcf5efa92
Simeon Widdis <sawiddis@gmail.com>
2025-02-16 19:24:34 -0800

    Begin proptest migration

--- tests/matching-tests.rs
+++ tests/matching-tests.rs
@@ -1,14 +1,48 @@
 use wordqat::SimplePattern;
 
-#[test]
-fn test_matches_crossword() {
-    let wordlist = ["lone", "love", "word", "door", "dome", "lint", "leftie"];
-    let pattern = SimplePattern::try_from("l..e").unwrap();
-    let result: Vec<&str> = wordlist
-        .into_iter()
-        .filter(|w| pattern.is_match(w))
-        .collect();
-    assert_eq!(result, vec!["lone", "love"])
+use proptest::prelude::*;
+
+#[derive(Debug, Clone)]
+enum ExpressionPart {
+    Letter(char),
+    Dot(char),
+    // Set(char, u32),
+}
+
+fn expression_part_strategy() -> impl Strategy<Value = ExpressionPart> {
+    prop_oneof![
+        prop::char::range('a', 'z').prop_map(ExpressionPart::Letter),
+        prop::char::range('a', 'z').prop_map(ExpressionPart::Dot),
+        // (prop::char::range('a', 'z'), prop::bits::u32::ANY).prop_map(|(a, b)| ExpressionPart::Set(a, b)),
+    ]
+}
+
+fn split_ex(part: ExpressionPart) -> (String, String) {
+    match part {
+        ExpressionPart::Letter(c) => (c.to_string(), c.to_string()),
+        ExpressionPart::Dot(c) => (".".to_string(), c.to_string()),
+        // ExpressionPart::Set(c, m) => todo!(),
+    }
+}
+
+proptest! {
+    #[test]
+    fn test_parse_doesnt_crash(s in "\\PC*") {
+        let _ = SimplePattern::try_from(s.as_str());
+    }
+
+    #[test]
+    fn test_matches_simple_expression(ex_parts in proptest::collection::vec(expression_part_strategy(), 0..50)) {
+        let (mut ex, mut word) = (String::new(), String::new());
+        for part in ex_parts {
+            let (exs, words) = split_ex(part);
+            ex.push_str(&exs);
+            word.push_str(&words);
+        }
+
+        let pattern = SimplePattern::try_from(ex.as_str()).unwrap();
+        prop_assert!(pattern.is_match(&word));
+    }
 }
 
 #[test]
