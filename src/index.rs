415419c7f807c1815dd77481e05c882b76b211ad
Simeon Widdis <sawiddis@gmail.com>
2025-02-16 23:59:38 -0800

    Alternative approach for search

--- src/index.rs
+++ src/index.rs
@@ -1,28 +1,35 @@
-use trie_rs::{Trie, TrieBuilder};
+use std::collections::BTreeMap;
 
-use crate::{pattern::Element, SimplePattern};
+#[derive(Debug)]
+struct Node {
+    eof: bool,
+    links: BTreeMap<char, usize>,
+}
 
 #[derive(Debug)]
 pub struct Index {
-    trie: Trie<u8>
+    nodes: Vec<Node>
 }
 
 impl Index {
     pub fn new(words: Vec<&str>) -> Self {
-        Self { trie: words.into_iter().collect() }
-    }
-
-    pub fn search(&self, pattern: &SimplePattern) -> Vec<&str> {
-        let mut results = Vec::new();
-        let mut inc_search = self.trie.inc_search();
-
-        for elem in pattern.sequence.iter() {
-            match elem {
-                Element::Set(mask) => {},
-                _ => todo!(),
+        let mut nodes = vec![Node { eof: false, links: BTreeMap::new() }];
+        for word in words {
+            let mut state = 0;
+            for c in word.chars() {
+                match nodes[state].links.get(&c) {
+                    Some(next) => state = *next,
+                    None => {
+                        let next = nodes.len();
+                        nodes[state].links.insert(c, next);
+                        nodes.push(Node { eof: false, links: BTreeMap::new() });
+                        state = next;
+                    }
+                }
             }
+            nodes[state].eof = true;
         }
 
-        results
+        Self { nodes }
     }
 }
